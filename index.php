cc0fd0e573059150ea935e3aa3cef03785314b71
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2024-10-01 19:51:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario PHP y HTML</title>
-    <link rel="stylesheet" href="stylesFORM.css">
 </head>
 <body>
     <h1>Formulario de Datos</h1>
